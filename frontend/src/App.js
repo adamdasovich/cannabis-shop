d4f8cf37dc29b604214cfe76ff18df5302b37296
adamdasovich <adamdasovich@gmail.com>
2022-08-30 13:00:28 -0400

    basic frontend working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ProductScreen from './screens/ProductScreen';
 
 
 const App = () => {
 	return (
-		<div className="App">
-			<h1>Adam's ProShop</h1>
-		</div>
+		<Router>
+			<Header />
+			<main className='py-3'>
+				<Container>
+					<Routes>
+						<Route exact path='/' element={<HomeScreen />} />
+						<Route exact path='/product/:id' element={<ProductScreen />} />
+					</Routes>
+				</Container>
+			</main>
+			<Footer />
+		</Router>
 	);
 }
 
