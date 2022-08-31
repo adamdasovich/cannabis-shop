bd9e575644653c37916493e1b03e53f3f1513f7e
adamdasovich <adamdasovich@gmail.com>
2022-08-31 14:02:16 -0400

    up to the end of Redux part 3, working

---
+++ frontend/src/reducers/productReducers.js
@@ -0,0 +1,15 @@
+import { PRODUCTS_LIST_FAILURE, PRODUCTS_LIST_REQUEST, PRODUCTS_LIST_SUCCESS } from '../constants/productConstants'
+
+
+export const productListReducer = (state = { products: [] }, action) => {
+	switch (action.type) {
+		case PRODUCTS_LIST_REQUEST:
+			return { loading: true, products: [] }
+		case PRODUCTS_LIST_SUCCESS:
+			return { loading: false, products: action.payload }
+		case PRODUCTS_LIST_FAILURE:
+			return { loading: false, error: action.payload }
+		default:
+			return state
+	}
+}
