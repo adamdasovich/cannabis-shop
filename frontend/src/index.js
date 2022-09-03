3c2ef082d4818c14402180d968be9bcf47ae6da3
adamdasovich <adamdasovich@gmail.com>
2022-09-03 09:59:13 -0400

    Revert "up to the end of Redux part 3, working"
    This reverts commit bd9e575644653c37916493e1b03e53f3f1513f7e.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import store from './store';
 import './bootstrap.min.css';
 import './index.css';
 import App from './App';
@@ -9,8 +7,8 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<Provider store={store}>
+	<React.StrictMode>
 		<App />
-	</Provider>
+	</React.StrictMode>
 );
 
