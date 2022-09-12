14471fd9ec9b4dbe987c9070f6ab4edb121b8adb
adamdasovich <adamdasovich@gmail.com>
2022-09-12 11:04:15 -0400

    rearranged file structure

--- seeder.js
+++ seeder.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import colors from "colors";
-import users from './data/users.js'
-import products from './data/products.js'
-import Product from './models/productModel.js'
-import User from './models/userModel.js'
-import Order from './models/orderModel.js'
+import users from './backend/data/users.js'
+import products from './backend/data/products.js'
+import Product from './backend/models/productModel.js'
+import User from './backend/models/userModel.js'
+import Order from './backend/models/orderModel.js'
 import connectDB from './config/db.js'
 
 dotenv.config()
